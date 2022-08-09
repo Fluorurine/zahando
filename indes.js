86a47a2c18e480ff96d7671b2c96ffc52337f421
Fluorurine <votruong830430073@gmail.com>
2022-08-09 19:55:15 +0700

    Init commit 4

--- indes.js
+++ indes.js
@@ -1,5 +1,11 @@
-<h1> D4C Za Warudo</h1>
-<h2>Cai nay la thu 2 ne</h>
-<h3>Za Warudo lan 3</h3>
-<h2>/lan4<h2>
-    <p>boy</p>
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
